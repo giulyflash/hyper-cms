475d24ea4f1f4df763c1e81eec18ce1a09b8819f
kulakov.serg@gmail.com <kulakov.serg@gmail.com@8f4f4d5b-203f-8297-4b56-ea31f1a16147>
2011-07-19 07:38:06 +0000

    module_link.php: repaired

--- project_kernel32/module/module_link/module_link.class.php
+++ project_kernel32/module/module_link/module_link.class.php
@@ -1,249 +1,202 @@
-$(function(){
-	admin_method_name = '_admin';
-	//sorting elements
-	sort_data();
-	//init module
-	$('.module_select').each(function(num,module_list){
-		module_list = $(module_list);
-		if(document.link_data)
-			get_module_name(module_list);
-		else{
-			module_name = '';
-			method_name = '';
-		}
-		for(i in document.module_data)
-			module_list.append('<option value="'+i+'" '+(module_name==i?'selected="1"':'')+'>'+document.module_data[i]['title']+'<optin/>');
-	});
-	if(document.link_data){
-		//init method
-		$('.method_select').each(function(num,method_list){
-			method_list = $(method_list);
-			get_module_name(method_list);
-			for(i in document.module_data[module_name]['method']){
-				method_list.append('<option value="'+i+'" '+(method_name==i?'selected="1"':'')+'>'+document.module_data[module_name]['method'][i]['title']+'<optin/>');
-			}
-			method_list.parent().parent().css('display','table-row');
-		});
-		//init params
-		$('.param_box').each(function(num,param_box){
-			param_box = $(param_box);
-			get_module_name(param_box);
-			for(i in document.link_data['param']){
-				if(document.link_data['param'][i]['type']==(is_condition?'condition':'param')){
-					param_box.append(get_param_td_html());
-					param_list = param_box.find('.param_select').last();
-					var parent_internal = parent;
-					param_list.change(function(){
-						param_change(parent_internal,$(this));
-					});
-					parameter_name = document.link_data['param'][i]['param_name'];
-					param_list.append('<option value="'+parameter_name+'" selected=1>'+document.module_data[module_name]['method'][method_name]['params'][parameter_name]+'</option>');
-					get_param_value(module_name,method_name,parameter_name,param_list);
-				}
-			}
-			param_change(parent,param_list,true);
-			param_box.parent().parent().css('display','table-row');
-		});
-	}
-	//event for module select
-	$('.module_select').change(function(){
-		parent = $(this).parent().parent().parent().parent();
-		if(!document.module_data){
-			alert('fatal error: data array not found!');
-			return;
-		}
-		value = $(this).val();
-		if(!value){
-			parent.find('.method, .params').css('display','none');
-			return;
-		}
-		if(!document.module_data[value]){
-			alert('fatal error: module "'+value+'" not found!');
-			return;
-		}
-		method_select = parent.find('.method_select');
-		method_select.html('<option></option>');
-		for(var method_name in document.module_data[value]['method']){
-			method_select.append('<option value="'+method_name+'">'+document.module_data[value]['method'][method_name]['title']
-			+(method_name==admin_method_name?' (admin)':'')+'</option>');
-		}
-		parent.find('.method').css('display','table-row');
-		parent.find('.params').css('display','none');
-	});
+<?php
+class module_link extends module{
+	protected $config_class_name = 'module_link_config';
 	
-	//event for method select
-	$('.method_select').change(function(){
-		parent = $(this).parent().parent().parent().parent();
-		if(!document.module_data){
-			alert('fatal error: data array not found!');
-			return;
-		}
-		method = $(this).val();
-		module = parent.find('.module_select').val();
-		//console.log(document.module_data[module]);return;
-		parent.find('.params').css('display','none');
-		if(!method || !document.module_data[module]['method'][method]['params'])
-			return;
-		parent.find('.params .param_box').html('');
-		param_change(parent);
-		parent.find('.params').css('display','table-row');
-	});
-});
-
-function param_change(parent,param_select,need_not_value){
-	module = parent.find('.module_select').val();
-	method = parent.find('.method_select').val();
-	param = param_select?($(param_select).val()):null;
-	//alert(module+'; '+method+'; '+param);
-	if(param_select){
-		if(param){
-			if(!need_not_value)
-				get_param_value(module,method,param,param_select);
+	public function &get_module($exclude = array(), $adnin_method_only=false){
+		//TODO check access for $module->admin_method
+		$exclude = array_merge($exclude, array('admin','app','base_module'));
+		$class_list = array();
+		$dir = _module_path;
+		if ($handle = opendir($dir)){
+			$module_root=scandir($dir);
+			foreach($module_root as $class_dir)
+				if(preg_match("%^([a-zA-Z\-_]+)$%",$class_dir,$re) && !in_array($re[1],$exclude)){
+					$module_name = $re[1];
+					$module = new $module_name($this->parent);
+					$callable_method = $module->_config('callable_method',true);
+					app::check_array_comma($callable_method);
+					if($adnin_method_only){
+						$admin_method = $module->_config('admin_method');
+						if($admin_method && isset($callable_method[$admin_method])){
+							$this->add_method($module, $admin_method, $class_list);
+						}
+					}
+					else{
+						$class_list[$module_name]['title'] = 
+							(!empty($this->parent->language_cache[$module_name][$title_str = $this->parent->_config('language_title_name')]))?
+								$this->parent->language_cache[$module->module_name][$title_str]:$module_name;
+						$params_title=$this->parent->_config('language_param_name');
+						$exclude_name = $this->parent->_config('exclude_method_from_link_list');
+						foreach(array_keys($callable_method) as $callable_name)
+							$this->add_method($module, $callable_name, $class_list, true, $callable_method, $title_str, $params_title, $exclude_name);
+					}
+				}
 		}
 		else
-			param_select.parent().parent().remove();
+			throw new exception('module directory not found',_module_path);
+		return $class_list;
 	}
-	select_list = parent.find('.params .param_box td:first-child .param_select');
-	param_count = 0;
-	for(param_name in document.module_data[module]['method'][method]['params'])
-		param_count++;
-	if((!select_list.size() || select_list.last().val())  && select_list.size()<param_count){
-		parent.find('.params .param_box').append(get_param_td_html());
-		parent.find('.params .param_box:last .param_select').change(function(){
-			param_change(parent,$(this));
-		});
-	}
-	param_all = parent.find('.params .param_box tr');
-	current_param_list = [];
-	if(param_select){
-		$.each(param_all, function(key,param_tr){
-			param_tr = $(param_tr);
-			select_each = param_tr.find('td:first-child select');
-			param_name = select_each.val();
-			select_each.attr('name',param_name?('link['+parent.index()+'][param]['+param_tr.index()+'][name]'):'');
-			if(param_name)
-				current_param_list[param_name] = 1;
-		});
-	}
-	option_html = [];
-	for(param_name in document.module_data[module]['method'][method]['params']){
-		if(!current_param_list[param_name])
-			option_html[param_name] = document.module_data[module]['method'][method]['params'][param_name];
-	}
-	param_all.each(function(key,tr_obj){
-		tr_obj = $(tr_obj);
-		p_select = tr_obj.find('td:first-child select').first();
-		p_select_value = p_select.val();
-		p_select.find('option').each(function(key,option){
-			option = $(option);
-			if(option.val() && p_select_value!=option.val())
-				option.remove();
-		});
-		for(param_name in option_html){
-			new_option = $("<option/>");
-			new_option.attr('value',param_name);
-			new_option.append(option_html[param_name]);
-			p_select.append(new_option);
-		}
-	});
-	$('.link_form .param_box').css('display','block');
-}
-
-function get_param_td_html(){
-	return '<tr><td><select class="param_select" autocomplete = "off"><option value=""></option></select></td><td></td></tr>';
-}
-
-function get_select_html(class_name,value,title){
-	return '<tr><td><select class="'+class_name+'" autocomplete = "off"><option></option>'+(value&&title?('<option value="'+value+'">'+title+'</option>'):'')+'</select></td><td></td></tr>';
-}
-
-function get_param_value(module,method,param,obj){
-	url_str = 'http://'+(window.location+'/').split( '/' )[2]+'/'+window.location.pathname+'?call='+module+'._get_param_value&_content=json&PHPSESSID='+PHPSESSID+'&method_name='+method+'&param_name='+param;
-	$.ajax({
-		url: url_str,
-		success: function(html){
-			//cache?
-			try{
-				eval('value_list = '+html);
-				if(value_list['__error']){
-					error_str = '';
-					for(error_module in value_list['__error'])
-						for(error_method in value_list['__error'][error_module])
-							for(error_i in value_list['__error'][error_module][error_method])
-								error_str += error_module+'.'+error_method+': '+value_list['__error'][error_module][error_method][error_i]['text']+"\n";
-					set_param_value(obj, null);
-					alert(error_str);
+	
+	public function add_method(&$module, &$method_name, &$class_list, $need_params = false, &$callable_method=NULL, &$title_str=NULL, &$params_title=NULL, &$exclude_name=NULL){
+		if($method_name && !$this->check_method_exclude($method_name, $callable_method) && method_exists($module, $method_name)){
+			$class_list[$module->module_name]['method'][$method_name]['title'] =
+				(!empty($this->parent->language_cache[$module->module_name][$method_name][$title = $this->parent->_config('language_title_name')]))?
+					($this->parent->language_cache[$module->module_name][$method_name][$title]):$method_name;
+			if($need_params){
+				$method_reflection = new ReflectionMethod($module,$method_name);
+				$params_reflection = $method_reflection->getParameters();
+				$params = &$class_list[$module->module_name]['method'][$method_name]['params'];
+				foreach($params_reflection as &$param_reflection){
+					$param_name = $param_reflection->name;
+					if(!isset($callable_method[$method_name][$param_name]) || $callable_method[$method_name][$param_name] && $callable_method[$method_name][$param_name]!=$exclude_name){
+						$params[$param_name] = !empty($this->parent->language_cache[$module->module_name][$method_name][$params_title][$param_name])?
+							$params[$param_name] = $this->parent->language_cache[$module->module_name][$method_name][$params_title][$param_name] : $param_name; 
+					}
 				}
-				else
-					set_param_value(obj, value_list);
 			}
-			catch(e){
-				alert(e);
-				alert('can not evaluete server data!');
+		}
+	}
+	
+	private function check_method_exclude($method, &$callable_method){
+		if(!empty($callable_method[$method][$this->parent->_config('exclude_method_from_link_list')]))
+			return true;
+	}
+
+	public function edit($id=NULL, $module=NULL, $method=NULL, $params=array()){
+		//module, method, params - to made link from
+		if($id){
+			$this->_result['link'] = $this->_query->select()->from('module_link')->where('id',$id)->query1();
+			$this->_result['link']['param'] = $this->_query->select()->from('module_link_param')->where('link_id',$id)->query();
+			$this->_result['link_position'] = $this->_result['link']['position']; 
+			$this->_result['link'] = json_encode($this->_result['link']);
+		}
+		$module_list = $this->get_module($this->_config('exclude_from_admin_list'));
+		$this->_result['data'] = json_encode($module_list);
+		$this->_result['position'] = $this->_query->select('translit_title,title')->from('position')->query2assoc_array('translit_title','title');
+	}
+	
+	public function save($id=NULL,$link=NULL,$position=NULL,$order=NULL){
+		//var_dump($link);
+		if(empty($link[1]['module']))
+			throw new my_exception('module name not found');
+		$link_value = array('module_name'=>$link[1]['module']);
+		if(!empty($link[1]['method']))
+			$link_value['method_name'] = $link[1]['method'];
+		if(!empty($link[2]['module']))
+			$link_value['center_module'] = $link[2]['module'];
+		if(!empty($link[2]['module']))
+			$link_value['center_method'] = $link[2]['method'];
+		$link_value['position'] = $position?$position:$this->parent->_config('main_position_name');
+		$link_value['order'] = $order?$order:1;
+		if($id){
+			$this->_query->update($this->module_name)->set($link_value)->where('id',$id)->limit(1)->execute();
+			$message = 'edit successfool';
+		}
+		else{
+			$this->_query->insert($this->module_name)->values($link_value)->execute();
+			if(!$id = $this->_query->insert_id())
+				throw new my_exception('id not found');
+			$message = 'add successfool';
+		}
+		foreach($link as $link_id=>&$link_item){
+			$this->_query->delete()->from($this->module_name.'_param')->where('link_id',$id)->query();
+			if(isset($link_item['param']))
+				foreach($link_item['param'] as &$param){
+					$type = ($link_id-1)?'condition':'param';
+					//var_dump($link,$link_id,$type);
+					$param_value = array('param_name'=>$param['name'], 'type'=>$type, 'link_id'=>$id);
+					if(isset($param['value']))
+						$param_value['value'] = $param['value'];
+					$this->_query->insert($this->module_name.'_param')->values($param_value)->execute();
+				}
+		}
+		$this->_message($message);
+		$this->parent->redirect('admin.php?call='.$this->module_name.'.'.$this->_config('admin_method'));
+	}
+	
+	public function _admin(){
+		$this->_result = $this->_query->select()->from('module_link')->where('menu',0)->order('order')->query2assoc_array('id',NULL,false);
+		$params = $this->_query->select()->from('module_link_param')->where('link_id',array_keys($this->_result),'in')->query();
+		foreach($params as &$param)
+			$this->_result[$param['link_id']]['params'][] = $param;
+		$title_field = $this->parent->_config('language_title_name');
+		$param_field =  $this->parent->_config('language_param_name');
+		$position = $this->_query->select('translit_title,title')->from('position')->query2assoc_array('translit_title','title');
+		foreach($this->_result as &$link){
+			$link['position_title'] = (!empty($position[$link['position']]))?$position[$link['position']]:$link['position'];
+			$this->get_link_translation($link,$title_field,$param_field);
+			$this->get_link_translation($link,$title_field,$param_field,1);
+		}
+	}
+	
+	private function get_link_translation(&$link,$title_field,$param_field,$canter_module = NULL){
+		if($canter_module){
+			$module_param = 'center_module';
+			$method_param = 'center_method';
+			$type_param = 'condition';
+			$module_trans_title = 'center_module_title';
+			$method_trans_title = 'center_method_title';
+		}
+		else{
+			$module_param = 'module_name';
+			$method_param = 'method_name';
+			$type_param = 'param';
+			$module_trans_title = 'module_title';
+			$method_trans_title = 'method_title';
+		}
+		$module_name = $link[$module_param];
+		$module = new $module_name($this->parent);
+		$link[$module_trans_title] = (!empty($this->parent->language_cache[$link[$module_param]][$title_field]))?
+		$this->parent->language_cache[$link[$module_param]][$title_field]:$link[$module_param];
+		$link[$method_trans_title] = (!empty($this->parent->language_cache[$link[$module_param]][$link[$method_param]][$title_field]))?
+		$this->parent->language_cache[$link[$module_param]][$link[$method_param]][$title_field]:$link[$method_param];
+		if(isset($link['params']))
+		foreach($link['params'] as &$param)
+			if($param['type']==$type_param){
+				$param['title'] = (!empty($this->parent->language_cache[$link[$module_param]][$link[$method_param]][$param_field][$param['param_name']]))?
+				$this->parent->language_cache[$link[$module_param]][$link[$method_param]][$param_field][$param['param_name']]:$param['param_name'];
+				$module->_get_param_value($link[$method_param],$param['param_name']);
+				if($module->_result)
+				if(!empty($module->_result[$param['value']]))
+				$param['value'] = $module->_result[$param['value']];
 			}
-		},
-		error: function(html){alert('can not recieve data from server!');}
-	});
-}
-
-function set_param_value(obj, value){
-	obj.parent().next().html(null);
-	parent = obj.parent().parent().parent().parent().parent().parent().parent().parent();
-	name_str = ' name="link['+parent.index()+'][param]['+obj.parent().parent().index()+'][value]"';
-	new_input = null;
-	for(param_dinamic in value){
-		if(!new_input)
-			new_input = $('<select'+name_str+'/>');
-		new_input.append('<option value="'+param_dinamic+'">'+value[param_dinamic]+'<optin/>');
 	}
-	if(!new_input)
-		new_input = $('<input type="text"'+name_str+'/>');
-	obj.parent().next().append(new_input);
-	new_input.focus();
-}
-
-function sort_data(){
-	for(var module_name in document.module_data)
-		document.module_data[module_name]['method'] = sort_by_title(document.module_data[module_name]['method']);
-	document.module_data = sort_by_title(document.module_data);
-}
-
-function sort_by_title(values){
-    sorted_keys = [];
-    sorted_obj = {};
-    for (var key in values)
-        sorted_keys.push([key,values[key]['title']]);
-    sorted_keys.sort(function(a,b){
-    	if(a[1]>b[1])
-    		return 1;
-    	else if(a[1]<b[1])
-    		return -1;
-    });
-    for (var key in sorted_keys)
-        sorted_obj[sorted_keys[key][0]] = values[sorted_keys[key][0]];
-    return sorted_obj;
-}
-
-function sort_obj(arr){
-    var sortedKeys = [];
-    var sortedObj = {};
-    for (var i in arr)
-        sortedKeys.push(i);
-    sortedKeys.sort();
-    for (var i in sortedKeys)
-        sortedObj[sortedKeys[i]] = arr[sortedKeys[i]];
-    return sortedObj;
-}
-
-function get_module_name($obj){
-	parent = $obj.parent().parent().parent().parent();
-	is_condition = parent.index()-1;
-	if(is_condition){
-		module_name = document.link_data['center_module'];
-		method_name = document.link_data['center_method'];
-	}
-	else{
-		module_name = document.link_data['module_name'];
-		method_name = document.link_data['method_name'];
+	
+	public function remove($id=NULL){
+		if(!$id)
+			throw new my_exception('id not found');
+		$this->_query->delete()->from($this->module_name)->where('id',$id)->limit(1)->execute();
+		$this->_query->delete()->from($this->module_name.'_param')->where('link_id',$id)->execute();
+		$this->_message('delete successfool');
+		$this->parent->redirect('admin.php?call='.$this->module_name.'.'.$this->_config('admin_method'));
 	}
 }
+
+class module_link_config extends module_config{
+	public $callable_method=array(
+		'_admin,edit,remove,save'=>array(
+			self::object_name=>__CLASS__,
+			self::role_name=>self::role_write,
+		),
+		'save'=>array(
+			'link'=>'_disable_filter',
+			'_exclude'=>true
+		),
+	);
+	
+	protected $template_include = array(
+		'module/module_link/link_wizard.xhtml.xsl',
+	);
+	
+	protected $exclude_from_admin_list = array();
+	
+	protected $include = array(
+		'_admin,edit'=>
+			'<link href="module/module_link/admin.css" rel="stylesheet" type="text/css"/>
+			<script type="text/javascript" src="module/module_link/admin.js"></script>',
+		'edit'=>
+			'<link href="module/module_link/wizard.css" rel="stylesheet" type="text/css"/>
+			<script type="text/javascript" src="module/module_link/wizard.js"></script>',
+	);
+}
+?>
