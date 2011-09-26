feed785ff54331af337ef430eb57a11ad076ed74
kulakov.serg@gmail.com <kulakov.serg@gmail.com@8f4f4d5b-203f-8297-4b56-ea31f1a16147>
2011-09-26 08:37:31 +0000

    category: slide slow

--- project_kernel32/module/gallery/gallery.js
+++ project_kernel32/module/gallery/gallery.js
@@ -3,6 +3,11 @@ $(function(){
 });
 
 function gallery_add_event(obj){
+	if(obj){
+		if(obj.css('display')=='none')
+			obj.parent().find('>.category_content').toggle('slow');
+		$('.gallery .loading').css('display','none');
+	}
 	if(!obj)
 		obj = $('.gallery');
 	obj.find('.img a').lightBox({
@@ -14,13 +19,22 @@ function gallery_add_event(obj){
 		txtOf: 'из'
 	});
 	obj.find('li a').click(function(){
-		var obj = $(this);
-		obj[0].opened = obj[0].opened?0:1;
-		obj.find('>img').attr('src',obj[0].opened?'module/base_module/img/category_up.png':'module/base_module/img/category_down.png');
-		var content = obj.parent().find('.category_content');
-		if(content.css('display')!='block')
-			_get_content(obj.attr('href'),content,'gallery_add_event');
-		content.toggle();
+		var obj_a = $(this);
+		obj_a[0].opened = obj_a[0].opened?0:1;
+		obj_a.find('>img:first').attr('src',obj_a[0].opened?'module/base_module/img/category_up.png':'module/base_module/img/category_down.png');
+		var content = obj_a.parent().find('.category_content');
+		if(content.css('display')!='block'){
+			if(content.html().length)
+				content.toggle('slow');
+			var loading = obj_a.find('.loading:last');
+			if(loading[0])
+				loading.css('display','inline');
+			else
+				obj_a.append('<img class="loading" src="module/module_link/img/loading1.gif"/>');
+			_get_content(obj_a.attr('href'),content,'gallery_add_event');
+		}
+		else
+			content.toggle('slow');
 		return false;
 	});
 }
