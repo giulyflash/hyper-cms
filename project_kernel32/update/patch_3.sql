8c97420865f5ea134198346826d868f4b6e90761
kulakov.serg <kulakov.serg@gmail.com>
2012-03-07 12:08:09 +0000

    patch files, jkomfort style

--- project_kernel32/update/patch_3.sql
+++ project_kernel32/update/patch_3.sql
@@ -1,3 +1,5 @@
+#ALTER TABLE `module_param` CHANGE `name` `param_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL 
+
 ALTER TABLE `module_link_param` CHANGE `param_name` `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `module_param` CHANGE `param_name` `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
@@ -45,6 +47,7 @@ UPDATE `article` SET `category_left` = (select `left` from article_category wher
 ALTER TABLE `article` DROP `category_id`;
 
 ALTER TABLE `article_category` ADD `category_count` INT NULL DEFAULT NULL;
+
 CREATE TEMPORARY TABLE `temp_category` (`left` int(11),`right` int(11),`depth` int(11));
 INSERT INTO temp_category (SELECT `left`,`right`,`depth` FROM `article_category`);
 UPDATE `article_category` SET `category_count` = (SELECT count(*) FROM `temp_category` WHERE `temp_category`.`left`>`article_category`.`left` AND `temp_category`.`right`>`article_category`.`right` AND `temp_category`.`depth`=`article_category`.`depth`+1);
@@ -58,13 +61,11 @@ UPDATE `article_category` SET `link`=concat('/',`translit_title`,'/');
 ALTER TABLE `article` DROP `id`;
 ALTER TABLE `article` CHANGE `translit_title` `id` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
 ALTER TABLE `article` ADD PRIMARY KEY ( `id` );
-ALTER TABLE `article` DROP INDEX `translit_title`;
 ALTER TABLE `article` CHANGE `category_left` `category_id` VARCHAR( 255 ) NULL DEFAULT NULL;
 
 ALTER TABLE `article_category` DROP `id`;
 ALTER TABLE `article_category` CHANGE `translit_title` `id` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
 ALTER TABLE `article_category` ADD PRIMARY KEY(`id`);
-ALTER TABLE `article_category` DROP INDEX translit_title`;
 ALTER TABLE `article_category` ADD INDEX ( `draft` );
 
 UPDATE article SET `category_id` = ( SELECT id FROM article_category WHERE id = category_id );
@@ -76,13 +77,26 @@ UPDATE `module_link` SET `center_method` = 'get' WHERE `center_method`='get_by_t
 #menu
 
 ALTER TABLE `menu_item` ADD `category_count` INT NULL DEFAULT NULL;
+
+DROP TABLE `temp_category`;
 CREATE TEMPORARY TABLE `temp_category` (`left` int(11),`right` int(11),`depth` int(11));
 INSERT INTO temp_category (SELECT `left`,`right`,`depth` FROM `menu_item`);
 UPDATE `menu_item` SET `category_count` = (SELECT count(*) FROM `temp_category` WHERE `temp_category`.`left`>`menu_item`.`left` AND `temp_category`.`right`>`menu_item`.`right` AND `temp_category`.`depth`=`menu_item`.`depth`+1);
 
 ALTER TABLE `menu_item` DROP `id`;
-ALTER TABLE `menu_item` CHANGE `translit_title` `id` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+UPDATE `menu_item` SET `translit_title` = `link`;
+ALTER TABLE `menu_item` CHANGE `translit_title` `id` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `menu_item` ADD PRIMARY KEY ( `id` );
+#left and right?
 
 ALTER TABLE `menu` ADD `order` INT NOT NULL DEFAULT '0';
 UPDATE `menu` SET `order` = `id`;
+
+CREATE TABLE `question` (
+	`id` INT( 255 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+	`text` VARCHAR( 255 ) NULL DEFAULT NULL ,
+	`create_date` DATETIME NULL DEFAULT NULL ,
+	`owner` VARCHAR( 255 ) NULL DEFAULT NULL ,
+	`edit_date` DATETIME NULL DEFAULT NULL ,
+	`draft` INT NULL DEFAULT NULL
+) ENGINE = InnoDB;
